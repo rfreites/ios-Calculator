cebbd32428373dc445439089702bc6a741ed0a36
RONNY FREITES <ronnyangelo.freites@gmail.com>
2019-05-31 16:38:34 -0600

    Initial commit

---
+++ ios-Calculator/Controllers/HomeController/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  ios-Calculator
+//
+//  Created by Ronny Freites on 5/31/19.
+//  Copyright © 2019 Ronny Freites. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    // MARK: Initialization
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
