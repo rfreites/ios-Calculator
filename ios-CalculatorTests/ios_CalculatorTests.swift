295be2ff13f4fe221ba5e53d73d10633dbc52695
RONNY FREITES <ronnyangelo.freites@gmail.com>
2019-05-31 09:26:19 -0600

    Initial Commit

---
+++ ios-CalculatorTests/ios_CalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_CalculatorTests.swift
+//  ios-CalculatorTests
+//
+//  Created by Ronny Freites on 5/31/19.
+//  Copyright © 2019 Ronny Freites. All rights reserved.
+//
+
+import XCTest
+@testable import ios_Calculator
+
+class ios_CalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
